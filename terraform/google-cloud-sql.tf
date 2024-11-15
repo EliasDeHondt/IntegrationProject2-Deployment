c69875a23c9078ae4ef7ac84864375786aa00f20
EliasDeHondt <Elias.dehondt@outlook.com>
2024-11-15 10:47:02 +0100

    Add Terraform configurations for Google Cloud SQL and Kubernetes cluster

--- terraform/google-cloud-sql.tf
+++ terraform/google-cloud-sql.tf
@@ -1,1 +1,15 @@
-# Description: 
+# Description: This file contains the configuration for the Google Cloud SQL instance.
+resource "google_sql_database_instance" "database-postgresql" {
+    name = "database-postgresql"
+    region = var.datacenter.region
+    database_version = var.database.database_version
+    settings {
+        tier = var.database.tier
+    }
+}
+
+resource "google_sql_user" "database-user" {
+    name = "database-user"
+    instance = var.google_sql_database_instance.database-postgresql.name
+    password = var.database_password
+}
