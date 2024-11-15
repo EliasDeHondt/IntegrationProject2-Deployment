f04ef4e3f282e8415b7760ba6c12d3c1dc064934
EliasDeHondt <Elias.dehondt@outlook.com>
2024-11-15 10:47:02 +0100

    Add Terraform configurations for Google Cloud SQL and Kubernetes cluster

--- terraform/kubernetes-cluster.tf
+++ terraform/kubernetes-cluster.tf
@@ -1,1 +1,19 @@
-# Description: 
+# Description: This file contains the configuration for the Kubernetes cluster.
+resource "google_container_cluster" "kubernetes-cluster" {
+    name = var.kubernetes.cluster_name
+    location = var.datacenter.region
+    initial_node_count = var.kubernetes.node_count
+    node_config {
+        machine_type = var.kubernetes.node_config.machine_type²
+        disk_size_gb = var.kubernetes.node_config.disk_size_gb
+        disk_type = var.kubernetes.node_config.disk_type
+        oauth_scopes = [
+            "https://www.googleapis.com/auth/devstorage.read_only",
+            "https://www.googleapis.com/auth/logging.write",
+            "https://www.googleapis.com/auth/monitoring",
+            "https://www.googleapis.com/auth/servicecontrol",
+            "https://www.googleapis.com/auth/service.management.readonly",
+            "https://www.googleapis.com/auth/sqlservice.admin"
+        ]
+    }
+}
--- terraform/var.tfvars
+++ terraform/var.tfvars
@@ -2,7 +2,24 @@
 projectid = "integrationproject2-deployment"
 credentials_file = "credentials.json"
 
+database_password = "123"
+
 datacenter = {
     region = "us-central1"
     zone = "us-central1-c"
+}
+
+database = {
+    database_version = "POSTGRES_17"
+    tier = "db-f1-micro"
+}
+
+kubernetes = {
+    cluster_name = "cluster-1"
+    node_count = 3
+    node_config = {
+        machine_type = "n1-standard-1"
+        disk_size_gb = 20
+        disk_type = "pd-standard"
+    }
 }
