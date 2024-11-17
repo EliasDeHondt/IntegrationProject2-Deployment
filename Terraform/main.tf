1d7d050e9cf16d57fd05717700967c39453b432e
EliasDeHondt <Elias.dehondt@outlook.com>
2024-11-17 19:55:21 +0100

    Add Kubernetes pod configurations and Terraform setup for project infrastructure

---
+++ Terraform/main.tf
@@ -0,0 +1,21 @@
+###########################
+# @author Elias De Hondt  #
+# @since 18/11/2024       #
+###########################
+# Description: This file contains the provider configuration for the Google Cloud Platform.
+terraform {
+    required_providers {
+        google = {
+            source = "hashicorp/google"
+            version = "3.5.0"
+        }
+    }
+}
+
+provider "google" {
+    credentials = file(var.credentials_file)
+
+    project = var.projectid
+    region = var.datacenter.region
+    zone = var.datacenter.zone
+}
