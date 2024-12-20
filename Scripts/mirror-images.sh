0d452502d5f99ed42f61533c1f76ae3e22630731
EliasDeHondt <Elias.dehondt@outlook.com>
2024-12-20 18:00:26 +0100

    Add cleanup commands to mirror-images.sh for local image removal

--- Scripts/mirror-images.sh
+++ Scripts/mirror-images.sh
@@ -16,6 +16,10 @@ sudo docker tag registry.gitlab.com/kdg-ti/integratieproject-2/2024-2025/team5/s
 # Push images to GitHub Container Registry
 sudo docker push ghcr.io/eliasdehondt/team5-ip2-frontend:latest
 sudo docker push ghcr.io/eliasdehondt/team5-ip2-backend:latest
+
+# Remove images from local machine
+sudo docker rmi registry.gitlab.com/kdg-ti/integratieproject-2/2024-2025/team5/react-frontend:latest
+sudo docker rmi registry.gitlab.com/kdg-ti/integratieproject-2/2024-2025/team5/spring-backend:latest
 ########################### TEAM05 ###########################
 
 ########################### TEAM06 ###########################
@@ -36,6 +40,12 @@ sudo docker push ghcr.io/eliasdehondt/team6-ip2-frontend:latest
 sudo docker push ghcr.io/eliasdehondt/team6-ip2-backend:latest
 sudo docker push ghcr.io/eliasdehondt/team6-ip2-ai:latest
 sudo docker push ghcr.io/eliasdehondt/team6-ip2-ollama:latest
+
+# Remove images from local machine
+sudo docker rmi registry.gitlab.com/kdg-ti/integratieproject-2/2024-2025/team6/frontend:latest
+sudo docker rmi registry.gitlab.com/kdg-ti/integratieproject-2/2024-2025/team6/backend-game-service/spring-backend:latest
+sudo docker rmi registry.gitlab.com/kdg-ti/integratieproject-2/2024-2025/team6/backend-ai-service/app:latest
+sudo docker rmi registry.gitlab.com/kdg-ti/integratieproject-2/2024-2025/team6/backend-ai-service/ollama:latest
 ########################### TEAM06 ###########################
 
 ########################### TEAM09 ###########################
@@ -56,6 +66,12 @@ sudo docker push ghcr.io/eliasdehondt/team9-ip2-frontend:compose
 sudo docker push ghcr.io/eliasdehondt/team9-ip2-frontend:kubernetes
 sudo docker push ghcr.io/eliasdehondt/team9-ip2-backend:latest
 sudo docker push ghcr.io/eliasdehondt/team9-ip2-ai:latest
+
+# Remove images from local machine
+sudo docker rmi registry.gitlab.com/kdg-ti/integratieproject-2/2024-2025/team9/frontend/react-frontend:local
+sudo docker rmi registry.gitlab.com/kdg-ti/integratieproject-2/2024-2025/team9/frontend/react-frontend:production
+sudo docker rmi registry.gitlab.com/kdg-ti/integratieproject-2/2024-2025/team9/backend-game-service/spring-backend:latest
+sudo docker rmi registry.gitlab.com/kdg-ti/integratieproject-2/2024-2025/team9/backend-ai-service/python-ai:latest
 ########################### TEAM09 ###########################
 
 ########################### TEAM20 ###########################
@@ -73,4 +89,9 @@ sudo docker tag registry.gitlab.com/kdg-ti/integratieproject-2/2024-2025/team20/
 sudo docker push ghcr.io/eliasdehondt/team20-ip2-frontend:latest
 sudo docker push ghcr.io/eliasdehondt/team20-ip2-backend:latest
 sudo docker push ghcr.io/eliasdehondt/team20-ip2-ai:latest
+
+# Remove images from local machine
+sudo docker rmi registry.gitlab.com/kdg-ti/integratieproject-2/2024-2025/team20/frontend/react-frontend-isb:latest
+sudo docker rmi registry.gitlab.com/kdg-ti/integratieproject-2/2024-2025/team20/backend-game-service/spring-backend:latest
+sudo docker rmi registry.gitlab.com/kdg-ti/integratieproject-2/2024-2025/team20/backend-ai-service/python-ai:latest
 ########################### TEAM20 ###########################
